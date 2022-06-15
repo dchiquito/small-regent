1b4b0cf09882e2259f204648573c2576be245145
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-06-15 14:11:43 -0400

    Animated tutorial

--- scripts/TutorialText.gd
+++ scripts/TutorialText.gd
@@ -1,25 +1,31 @@
 extends RichTextLabel
 
+onready var animation = $AnimationPlayer
+
 export var event: String
 export var visible_after: String = ''
 
+enum State {
+	WAITING,
+	VISIBLE,
+	EXPIRED,
+}
+var state = State.WAITING
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	percent_visible = 0
 	if visible_after == '':
 		# No triggers, this should be visible by default
-		visible = true
-	else:
-		visible = false
+		state = State.VISIBLE
+		animation.play("fade_in")
 
 func _input(_ev):
-	if Input.is_action_just_pressed(event):
+	if state == State.VISIBLE and Input.is_action_just_pressed(event):
 		# Player has succesfully followed the tutorial, no need to stick around
-		visible = false
-		queue_free()
-	if Input.is_action_just_pressed(visible_after):
+		state = State.EXPIRED
+		animation.play_backwards("fade_in")
+	if state == State.WAITING and Input.is_action_just_pressed(visible_after):
 		# Necessary conditions have been met, appear
-		visible = true
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		state = State.VISIBLE
+		animation.play("fade_in")
