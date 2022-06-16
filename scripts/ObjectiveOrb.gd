d462935d4e52a7c1d2ff9e8dc90cfbd7f6002988
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-06-15 23:04:06 -0400

    Hide orbs when collected

--- scripts/ObjectiveOrb.gd
+++ scripts/ObjectiveOrb.gd
@@ -6,5 +6,6 @@ func _on_ObjectiveOrb_body_entered(body):
 	if body.name == 'Player':
 		body.collect_orb()
 		sound.play()
+		visible = false
 		yield(get_tree().create_timer(1.1), "timeout")
 		queue_free()
